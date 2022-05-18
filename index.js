f41779a485acf3e1c0bb9ec268fd3c2f5fd19b5a
unknown <latapata1952@gmail.com>
2022-05-19 03:14:26 +0600

    database connected

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // require express, cors, mongodb and dotenv to secure database pass
 const express = require('express');
 const cors = require('cors');
-// const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 
 
@@ -15,6 +15,57 @@ const port = process.env.PORT || 5000;
 // use middleware
 app.use(cors());
 app.use(express.json());
+
+
+
+
+// connect with mongo database
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zioi5.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+// set connection function
+
+async function run() {
+    try {
+        await client.connect();
+
+        // tasks collection
+        const taskCollection = client.db("to_do_app").collection("tasks");
+
+
+        // tasks collection API
+
+        // Make API : get all task data from server
+        // link: http://localhost:5000/tasks
+
+        app.get('/tasks', async (req, res) => {
+            const query = {};
+            const cursor = taskCollection.find(query);
+            const tasks = await cursor.toArray();
+            res.send(tasks);
+        })
+
+
+        // POST item : add a new task
+        // link: http://localhost:5000/tasks
+
+        app.post('/tasks', async (req, res) => {
+            const newTask = req.body;
+            const result = await taskCollection.insertOne(newTask);
+            res.send(result);
+        })
+
+    }
+    finally {
+        // client.close();
+    }
+}
+
+
+run().catch(console.dir);
+
 
 
 
